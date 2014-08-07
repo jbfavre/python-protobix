666985424b7b5961f8e9250ecbc6bb3805dbf8c3
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2014-08-07 14:46:44 +0200

    Imported Upstream version 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'protobix',
-    version = '0.0.3',
+    version = '0.0.4',
     zip_safe = True,
     
     author = 'Jean Baptiste Favre',
