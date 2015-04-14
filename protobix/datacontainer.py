d287b87066f210f7eab5d9b1ef60b7442b02da36
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2015-04-14 12:14:18 +0200

    Initial commit. Migrated from old repository

---
+++ protobix/datacontainer.py
@@ -0,0 +1,38 @@
+import simplejson
+import time
+
+from senderprotocol import SenderProtocol
+
+class DataContainer(SenderProtocol):
+
+    def __init__(self, data_type=None, zbx_host="", zbx_port=10051):
+        super( DataContainer, self).__init__()
+        self.request = "sender data"
+        self.zbx_host = zbx_host
+        self.zbx_port = zbx_port
+        self.items_list = []
+        self.data_type = data_type
+
+    def set_type(self, data_type):
+        if data_type == "lld" or data_type == "items":
+            self.data_type = data_type
+
+    def add_item(self, host, key, value, clock=None):
+        if clock is None:
+            clock = int((time.time())/60*60)
+        if self.data_type == "items":
+            item = { "host": host, "key": key,
+                     "value": value, "clock": clock}
+        elif self.data_type == "lld":
+            item = { "host": host, "key": key, "clock": clock,
+                     "value": simplejson.dumps({"data":value}) }
+        self.items_list.append(item)
+
+    def add(self, data):
+        for host in data:
+            for key in data[host]:
+                if not data[host][key] == []:
+                    self.add_item( host, key, data[host][key])
+
+    def get_items_list(self):
+        return self.items_list
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'protobix',
+    packages = ['protobix'],
+    version = '0.0.5',
+
+    description = 'Implementation of Zabbix Sender protocol',
+    long_description = ( 'This module implements Zabbix Sender Protocol.\n'
+                         'It allows to build list of items and send items and send '
+                         'them as trapper.\n'
+                         'It currently supports items as well as Low Level Discovery.' ),    
+    author = 'Jean Baptiste Favre',
+    author_email = 'jean-baptiste.favre@blablacar.com',
+    license = 'GPL',
+    url='http://github.com/jbfavre/python-protobix/',
+    download_url = 'http://github.com/jbfavre/python-protobix/tarball/0.0.5',
+    keywords = ['monitoring','zabbix','trappers'],
+    classifiers = [],
+   )
