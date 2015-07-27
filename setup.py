02554fba86d18daee97ab43e9bb07d3bd941d5be
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2015-07-27 22:58:26 +0200

    Add tests into setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Command
+# you can also import from setuptools
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
 
 setup(
     name = 'protobix',
@@ -19,4 +34,5 @@ setup(
     download_url = 'http://github.com/jbfavre/python-protobix/tarball/0.0.8',
     keywords = ['monitoring','zabbix','trappers'],
     classifiers = [],
+    cmdclass={'test': PyTest},
    )
