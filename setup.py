cf742fa8ca184dcbc5fb3a275f8ddcc1c6335533
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2015-08-02 09:28:19 +0200

    Add gitginore file

--- setup.py
+++ setup.py
@@ -12,15 +12,17 @@ class PyTest(Command):
         pass
 
     def run(self):
-        import subprocess
-        import sys
-        errno = subprocess.call([sys.executable, 'runtests.py'])
+        import pytest
+        options = "--cov protobix --cov-report term-missing"
+        try: import coverage
+        except ImportError: options = ""
+        errno = pytest.main(options)
         raise SystemExit(errno)
 
 setup(
     name = 'protobix',
     packages = ['protobix'],
-    version = '0.0.8.1',
+    version = '0.0.9',
 
     description = 'Implementation of Zabbix Sender protocol',
     long_description = ( 'This module implements Zabbix Sender Protocol.\n'
@@ -31,7 +33,7 @@ setup(
     author_email = 'jean-baptiste.favre@blablacar.com',
     license = 'GPL',
     url='http://github.com/jbfavre/python-protobix/',
-    download_url = 'http://github.com/jbfavre/python-protobix/tarball/0.0.8.1',
+    download_url = 'http://github.com/jbfavre/python-protobix/tarball/0.0.9',
     keywords = ['monitoring','zabbix','trappers'],
     classifiers = [],
     cmdclass={'test': PyTest},
