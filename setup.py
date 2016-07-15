70dbb025bb47a793fb2f6430c20f76588dc966b7
Jean Baptiste Favre <debian@jbfavre.org>
2016-07-15 15:39:07 +0200

    Import python-protobix_0.1.2.orig.tar.gz

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(TestCommand):
 setup(
     name = 'protobix',
     packages = ['protobix'],
-    version = '0.1.1',
+    version = '0.1.2',
     install_requires = [
         'configobj',
         'simplejson',
@@ -40,7 +40,7 @@ setup(
     author_email = 'jean-baptiste.favre@blablacar.com',
     license = 'GPL-3+',
     url='https://github.com/jbfavre/python-protobix/',
-    download_url = 'https://github.com/jbfavre/python-protobix/archive/0.1.1.tar.gz',
+    download_url = 'https://github.com/jbfavre/python-protobix/archive/0.1.2.tar.gz',
     keywords = ['monitoring','zabbix','trappers'],
     classifiers = [],
     cmdclass={'test': PyTest}
