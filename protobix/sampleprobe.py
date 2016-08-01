5773e567a86aa470bbdbe1e6cf8e485237f39ec6
Jean Baptiste Favre <github@jbfavre.org>
2016-08-01 23:47:30 +0200

    Code refacto for SampleProbe
    * migration from optparse to argparse
    * fix lint issues

--- protobix/sampleprobe.py
+++ protobix/sampleprobe.py
@@ -1,11 +1,12 @@
 import logging
-import optparse
+import argparse
 import socket
 import sys
 import traceback
 from logging import handlers
 
-from .datacontainer import DataContainer
+#from .datacontainer import DataContainer
+import protobix
 
 class SampleProbe(object):
 
@@ -20,44 +21,67 @@ class SampleProbe(object):
     ]
     logger = None
     probe_config = None
+    hostname = None
+    options = None
 
     def _parse_args(self):
         # Parse the script arguments
         # Common part
-        parser = optparse.OptionParser()
+        parser = argparse.ArgumentParser(
+            usage='%(prog)s [options]',
+            description = 'A Protobix probe to monitor XXX with Zabbix',
+            epilog = 'Protobix - copyright 2016 - Jean Baptiste Favre (www.jbfavre.org)'
+        )
 
-        parser.add_option('-c', '--config', default = None,
-                          help='Probe config file location. '
-                               'Can be either absolute or relative path')
-        parser.add_option('-d', '--dry', action='store_true', default = False,
-                          help='Performs CDH API calls but do not send '
-                               'anything to the Zabbix server. Can be used '
-                               'for both Update & Discovery mode')
-        parser.add_option('-D', '--debug', action='store_true', default = False,
-                          help='Enable debug mode. This will prevent bulk send '
-                               'operations and force sending items one after the '
-                               'other, displaying result for each one')
+        common = parser.add_argument_group('Common options')
+        common.add_argument( '-c', '--config',
+            default = None,
+            help='Probe config file location. '
+                'Can be either absolute or relative path'
+        )
+        common.add_argument( '-d', '--dry',
+            action='store_true',
+            default = False,
+            help='Performs CDH API calls but do not send '
+                'anything to the Zabbix server. Can be used '
+                'for both Update & Discovery mode'
+        )
+        common.add_argument( '-D', '--debug',
+            action='store_true',
+            default = False,
+            help='Enable debug mode. This will prevent bulk send '
+                'operations and force sending items one after the '
+                'other, displaying result for each one'
+        )
+        common.add_argument( '--update-items',
+            action='store_const',
+            dest='protobix_mode',
+            const='update_items',
+            default='update_items',
+            help='Get & send items to Zabbix. This is the default '
+                'behaviour even if option is not specified'
+        )
+        common.add_argument( '--discovery',
+            action='store_const',
+            dest='mode', const='discovery',
+            help='If specified, will perform Zabbix Low Level '
+                'Discovery on Hadoop Cloudera Manager API. '
+                'Default is to get & send items'
+        )
 
-        zabbix_options = optparse.OptionGroup(parser, 'Zabbix configuration')
-        zabbix_options.add_option('-z', '--zabbix-server', default='127.0.0.1',
-                                  help='The hostname of Zabbix server or '
-                                       'proxy, default is 127.0.0.1.')
-        zabbix_options.add_option('-p', '--zabbix-port', default=10051,
-                                  help='The port on which the Zabbix server or '
-                                       'proxy is running, default is 10051.')
-        zabbix_options.add_option('--update-items', action='store_const',
-                                  dest='mode', const='update_items',
-                                  help='Get & send items to Zabbix. This is the default '
-                                       'behaviour even if option is not specified')
-        zabbix_options.add_option('--discovery', action='store_const',
-                                  dest='mode', const='discovery',
-                                  help='If specified, will perform Zabbix Low Level '
-                                       'Discovery on Hadoop Cloudera Manager API. '
-                                       'Default is to get & send items')
-        parser.add_option_group(zabbix_options)
-        parser.set_defaults(mode='update_items')
+        zabbix = parser.add_argument_group('Zabbix specific options')
+        zabbix.add_argument( '-z', '--zabbix-server',
+            default='127.0.0.1',
+            help='The hostname of Zabbix server or '
+                'proxy, default is 127.0.0.1.'
+        )
+        zabbix.add_argument( '-p', '--zabbix-port',
+            default=10051,
+            help='The port on which the Zabbix server or '
+                'proxy is running, default is 10051'
+        )
 
-        return parser
+        return parser.parse_args()
 
     def _setup_logging(self, zbx_container):
         logger = logging.getLogger(self.__class__.__name__)
@@ -116,19 +140,29 @@ class SampleProbe(object):
         )
         return zbx_container
 
+    def _get_metrics(self):
+        pass
+
+    def _get_discovery(self):
+        pass
+
+    def _init_probe(self):
+        pass
+
     def run(self):
-        (self.options, args) = self._parse_args()
-        self.hostname = socket.getfqdn()
+        self.options = self._parse_args()
 
         # Datacontainer init (needs logger)
         zbx_container = self._init_container()
+        self.hostname = zbx_container.hostname
+
         # logger init
         self.logger = zbx_container.logger = self._setup_logging(zbx_container)
 
         # Step 1: read probe configuration
         try:
             self._init_probe()
-        except Exception as e:
+        except:
             self.logger.error('Step 1 - Read probe configuration failed')
             self.logger.debug(traceback.format_exc())
             return 1
@@ -154,7 +188,7 @@ class SampleProbe(object):
             zbx_container.add(data)
         except Exception as e:
             self.logger.error(
-                'Step 3 - Format & add Data failed [%s]' % e.strerror
+                'Step 3 - Format & add Data failed [%s]' % str(e)
             )
             self.logger.debug(traceback.format_exc())
             return 3
@@ -168,11 +202,16 @@ class SampleProbe(object):
             )
             self.logger.debug(traceback.format_exc())
             return 4
-        except Exception as e:
+        except:
             self.logger.error(
-                'Step 4 - Unknown error [%s]' % e.strerror
+                'Step 4 - Unknown error [%s]' % str(e)
             )
             self.logger.debug(traceback.format_exc())
             return 4
         # Everything went fine. Let's return 0 and exit
         return 0
+
+if __name__ == '__main__':
+    ret = SampleProbe().run()
+    print ret
+    sys.exit(ret)
