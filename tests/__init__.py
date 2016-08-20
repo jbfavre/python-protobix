df733d0193fff54c910654b742e244cbee748561
Jean Baptiste Favre <github@jbfavre.org>
2016-08-20 22:13:21 +0200

    Update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,15 +2,19 @@
 
 import sys
 import pytest
+try:
+    import coverage
+    coverage_options = ['--cov', 'protobix', '--cov-report', 'term-missing']
+except ImportError:
+    coverage_options = []
+try:
+    import pylint
+    pylint_options = ['--pylint ', '--pylint-error-types=WEF']
+except ImportError:
+    pylint_options = []
 
-user_options = []
 pytest_options = ['-v', '-k-_need_backend']
-coverage_options = ['--cov', 'protobix', '--cov-report', 'term-missing']
-pylint_options = ['--pylint ', '--pylint-error-types=WEF']
-try: import coverage
-except ImportError: coverage_options = []
-try: import pylint
-except ImportError: pylint_options = []
 pytest_options += coverage_options
 pytest_options += pylint_options
+
 pytest.main(pytest_options)
