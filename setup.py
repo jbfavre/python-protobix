b4d00a9dd2ccfd9541c636e7218499ea46597d88
Jean Baptiste Favre <github@jbfavre.org>
2016-08-20 11:48:38 +0200

    Update requirements files. Update setup.py

--- setup.py
+++ setup.py
@@ -14,15 +14,17 @@ class PyTest(TestCommand):
 
     def run(self):
         import pytest
-        pytest_options = '-v -k-_need_backend'
-        coverage_options = ' --cov protobix --cov-report term-missing'
-        pylint_options = ' --pylint  --pylint-error-types=WEF'
-        pylint_options = ''
+        pytest_options = ['-v', '-k-_need_backend']
+        coverage_options = ['--cov', 'protobix', '--cov-report', 'term-missing']
+        pylint_options = ['--pylint ', '--pylint-error-types=WEF']
         try: import coverage
-        except ImportError: coverage_options = ''
+        except ImportError: coverage_options = ['']
         try: import pylint
-        except ImportError: pylint_options = ''
-        errno = pytest.main(pytest_options + coverage_options + pylint_options)
+        except ImportError: pylint_options = ['']
+        pytest_options += coverage_options
+        pytest_options += pylint_options
+        print(pytest_options)
+        errno = pytest.main(pytest_options)
         raise SystemExit(errno)
 
 setup(
