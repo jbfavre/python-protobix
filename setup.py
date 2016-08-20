2d66373eb701a96ada11dd0bfb97bb1c46e78914
Jean Baptiste Favre <github@jbfavre.org>
2016-08-20 12:12:56 +0200

    Fix test fail if options empty

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ class PyTest(TestCommand):
         coverage_options = ['--cov', 'protobix', '--cov-report', 'term-missing']
         pylint_options = ['--pylint ', '--pylint-error-types=WEF']
         try: import coverage
-        except ImportError: coverage_options = ['']
+        except ImportError: coverage_options = []
         try: import pylint
-        except ImportError: pylint_options = ['']
+        except ImportError: pylint_options = []
         pytest_options += coverage_options
         pytest_options += pylint_options
         print(pytest_options)
