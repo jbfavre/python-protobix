4b8c88e0cb4924adecbbe9c4ed6f38c3135146ab
Jean Baptiste Favre <debian@jbfavre.org>
2016-08-25 13:52:25 +0200

    Prepare 1.0.0alpha1. Closes #9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name = 'protobix',
     packages = ['protobix'],
-    version = '0.1.2',
+    version = '1.0.0alpha1',
     install_requires = [
         'configobj',
         'simplejson'
@@ -26,7 +26,7 @@ setup(
     author_email = 'jean-baptiste.favre@blablacar.com',
     license = 'GPL-3+',
     url='https://github.com/jbfavre/python-protobix/',
-    download_url = 'https://github.com/jbfavre/python-protobix/archive/0.1.2.tar.gz',
+    download_url = 'https://github.com/jbfavre/python-protobix/archive/1.0.0alpha1.tar.gz',
     keywords = ['monitoring','zabbix','trappers'],
     classifiers = [],
    )
